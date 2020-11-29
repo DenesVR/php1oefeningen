7880d648d0bce8ea2ebe5f299740723f06095725
DenesVR <denesvanroy@gmail.com>
2020-11-29 17:33:46 +0100

    img toevoegen & oplossing deel 1

--- oef2-1/steden.php
+++ oef2-1/steden.php
@@ -1,43 +1,50 @@
 <?php
-$steden = array("Berlijn"=>"img/berlijn.jpg","Parijs"=>"img/parijs.jpg","Londen"=>"img/londen.jpg")
-
+include_once "connection.php";
 ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <title>Oefening 1.2</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    </head>
-    <body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Oefening 1.2</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-    <div class="jumbotron text-center">
-        <h1>Leuke plekken in Europa</h1>
-        <p>Tips voor citytrips voor vrolijke vakantiegangers!</p>
-    </div>
+<div class="jumbotron text-center">
+    <h1>Leuke plekken in Europa</h1>
+    <p>Tips voor citytrips voor vrolijke vakantiegangers!</p>
+</div>
 
-    <div class="container">
-        <div class="row">
-            <?php
+<div class="container">
+    <div class="row">
+        <?php
 
-            foreach ($steden as $key => $value) {
+        $sql = "SELECT * FROM images";
+        $result = mysqli_query($conn,$sql);
+        $resultCheck = mysqli_num_rows($result);
+
+        if($resultCheck > 0) {
+            while ($row = mysqli_fetch_assoc($result)){
 
                 echo "<div class='col-sm-4'>";
-                echo "<h3>".$key."</h3>";
+                echo "<h3>".$row[img_title]."</h3>";
+                echo "<p>".$row[img_width]." x ".$row[img_height]."</p>";
                 echo "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>";
                 echo "<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>";
-                echo "<img src=" . $value . ">";
+                echo "<img src=" . $row[img_filename] . ">";
                 echo "</div>";
+
             }
+        }
 
-            ?>
-        </div>
+
+        ?>
     </div>
+</div>
 
-    </body>
-    </html>
-<?php
+</body>
+</html>
