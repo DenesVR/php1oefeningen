0108080505b967a1d047c37372b01faa9c124ab2
DenesVR <denesvanroy@gmail.com>
2020-12-03 12:14:49 +0100

    commentaar toegevoegd

--- oef2-2/lib/html_components.php
+++ oef2-2/lib/html_components.php
@@ -1,9 +1,11 @@
 <?php
 
+#Printen van de html head
 function printHead() {
     echo file_get_contents("templates/head.html");
 }
 
+#Printen van de Jumbotron html
 function printJumbo() {
     echo file_get_contents("templates/jumbo.html");
 }
